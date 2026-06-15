4ca0756aea22289d102a5f282ff97443393f6948
Nicholas Nicholas <nickolaosnick@gmail.com>
2026-06-15 23:50:56 +0100

    add test project structure

---
+++ MusicDiscovery/MusicDiscovery.Tests/Services/DeezerServiceTests.cs
@@ -0,0 +1,15 @@
+﻿namespace MusicDiscovery.Tests;
+
+public class DeezerServiceTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
